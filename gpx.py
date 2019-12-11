ad252a6fafff87983f05c55b60745ae047c99494
Wink Saville <wink@saville.com>
2019-12-11 13:08:07 -0800

    Get gpx.py running
    - Deleted the code that was moved to xml_element_tree.py
    
    - Determined that the ElementTree contains dictionaries not simple strings
      so the regex weren't needed.
    
    - Implemented parse_trkpt

--- gpx.py
+++ gpx.py
@@ -5,94 +5,48 @@ import math
 import numpy as np
 import xml.etree.ElementTree as et
 import track_point as tp
-import re
 
-#from __future__ import annotations
-from typing import Any, Union, Optional, Tuple, Pattern, Match
+from typing import Optional, Tuple
 
-tree = et.parse('RAAM_TS00_route_snippet.gpx')
-root: et.Element = tree.getroot()
-print(f'root.tag={root.tag}')
-print(f'root.attrib={root.attrib}')
+def parse_trkpt(elem_trkpt: et.Element) -> Optional[tp.TrackPoint]:
+    "Return lat, lon, ele"
+    lat_str: str = ''
+    lon_str: str = ''
 
-def parseTag(tag: str) -> Tuple[Optional[str], str]:
-    "Return ns, name"
-    if tag[0] == '{':
-        ns, name = tag[1:].split("}")
+    if not elem_trkpt:
+        return None
+
+    if elem_trkpt.attrib:
+        lat_str = elem_trkpt.attrib['lat']
+        lon_str = elem_trkpt.attrib['lon']
+        if not (lat_str and lon_str):
+            return None
     else:
-        ns, name = Tuple[None, tag]
-    return ns, name
+        return None
 
-def prtElement(level: int, elem: et.Element) -> None:
-    space = ' '
-    indent = 4
-    tagNs, tagName = parseTag(elem.tag)
-    print(f'{space:<{level}}{tagName}', end = '')
-    if elem.attrib:
-      print(f' attrib={elem.attrib}', end = '')
-    print(':')
-    if elem.text and elem.text.strip() != "":
-        print(f'{space:<{level+indent}}text="{elem.text}"')
-    if elem.tail and elem.tail.strip() != "":
-        print(f'{space:<{level+indent}}tail="{elem.tail}"')
-
-    for subElem in elem:
-        prtElement(level+indent, subElem)
-
-print('\nroot:')
-prtElement(0, root)
-print('root: DONE')
-
-print('\ntrack points:')
-#for elem_trkpt in root.findall('./{*}trk/{*}trkseg/{*}trkpt'):
-elem_trkpt: et.Element
-for elem_trkpt in root.findall('.//{*}trkpt'):
-    prtElement(0, elem_trkpt)
-
-print('track points: DONE')
-
-pt1 = tp.TrackPoint()
-print(f'pt1={pt1}')
-
-re_float: str = r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?'
-pi_str: str = '3.14159'
-cre_num: Pattern = re.compile(re_float)
-# How to define a user type for Optional[Match[str]]???
-pi_match_obj: Optional[Match[str]] = cre_num.match(pi_str)
-print(f'cre_num.match({pi_str})={pi_match_obj}')
-if pi_match_obj:
-    pi_match_obj_group = pi_match_obj.group()
-    print(f'pi_match.group()=={pi_match_obj_group}')
-    assert(pi_match_obj_group == pi_str)
-
-re_trkpt_lat_lon_attr: str = r'{\'lat\': \'(?P<lat>' + re_float + r')\', \'lon\': \'(?P<lon>' + re_float + r')\'}'
-cre_trkpt_lat_lon_attr: Pattern = re.compile(re_trkpt_lat_lon_attr)
-
-lat_lon_attr_str: str = r"{'lat': '1.0e1', 'lon': '-2.0e-1'}"
-lat_lon_match_obj: Optional[Match[str]] = cre_trkpt_lat_lon_attr.match(lat_lon_attr_str)
-print(f'lat_lon_match_obj={lat_lon_match_obj}')
-if lat_lon_match_obj:
-    lat_str: str
-    lon_str: str
-    lat_str, lon_str = lat_lon_match_obj.group('lat', 'lon')
-    print(f'lat_str={lat_str}, lon_str={lon_str}')
-    lat=float(lat_str)
-    lon=float(lon_str)
-    print(f'lat={lat}, lon={lon}')
-
-def parse_trkpt_attrib(elem) -> Tuple[Optional[str], str]:
-    "Return lat, lon"
-    if elem.attrib[0] == '{':
-        ns, name = elem[1:].split("}")
+    elem_ele = elem_trkpt.find('.//{*}ele')
+    ele_str: str
+    if elem_ele is not None and elem_ele.text:
+        ele_str = elem_ele.text.strip()
+        if ele_str == '':
+            ele_str = '0.0'
     else:
-        ns, name = None, elem
-    return ns, name
+        ele_str = '0.0'
 
-def gpx_trkpt_to_TrackPoint(elem_trkpt: et.Element) -> tp.TrackPoint:
-    return tp.TrackPoint()
+    return tp.TrackPoint(lat=float(lat_str), lon=float(lon_str), ele=float(ele_str))
 
-print('TrackPoints:')
-for elem_trkpt1 in root.findall('.//{*}trkpt'):
-    pt = gpx_trkpt_to_TrackPoint(elem_trkpt)
-    print(f'pt={pt}')
-print('TrackPoints: DONE')
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Display gpx trkpt's.")
+    parser.add_argument('filename', type=str, help='gpx file to process')
+    args = parser.parse_args()
+    print(f'filename={args.filename}')
+
+    elem_trkpt: et.Element
+    tree = et.parse(args.filename)
+    root: et.Element = tree.getroot()
+
+    for elem_trkpt in root.findall('.//{*}trkpt'):
+        pt = parse_trkpt(elem_trkpt)
+        print(f'pt={pt}')
